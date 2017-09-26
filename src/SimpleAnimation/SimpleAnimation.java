306e3a9b44febcbc2921ba09dd65dd39ef34cb7d
Br0n50n <marcinster@gmail.com>
2017-09-26 22:53:04 +0200

    Gui Simple Animation

---
+++ src/SimpleAnimation/SimpleAnimation.java
@@ -0,0 +1,44 @@
+package SimpleAnimation;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SimpleAnimation {
+
+    int x = 70;
+    int y = 70;
+
+    public static void main(String[] args) {
+        SimpleAnimation gui = new SimpleAnimation();
+        gui.click();
+    }
+
+    public void click(){
+        JFrame ramka = new JFrame();
+        ramka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        PanelRysunkowy panel = new PanelRysunkowy();
+
+        ramka.getContentPane().add(panel);
+        ramka.setSize(300, 300);
+        ramka.setVisible(true);
+
+        for (int i = 0; i < 130 ; i++) {
+            x++;
+            y++;
+
+            ramka.repaint();
+            try {
+                Thread.sleep(50);
+            } catch (Exception ex) { }
+        }
+
+    }
+
+    class PanelRysunkowy extends JPanel{
+        public void paintComponent(Graphics g){
+            g.setColor(Color.green);
+            g.fillOval(x,y, 40, 40);
+        }
+    }
+}
